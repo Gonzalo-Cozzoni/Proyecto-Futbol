8998dd24d83fdab6c9e3e03e1608820d6f3695ce
Gonzalo <gonzalod88@gmail.com>
2024-04-20 23:28:48 -0300

    pasamos el menu a js

--- js/principal.js
+++ js/principal.js
@@ -13,3 +13,58 @@ document.write(`<header>
     </ul>
 </nav>
 </header>`)
+
+document.write(`<main>
+<section id="banner">
+    <h1 id="titulo">Reserva tu cancha AHORA</h1>
+    
+</section>
+<section id="carrusel">
+    <div id="contenedor_carrusel">
+        <div class="item_carrusel" id="carrusel_foto1">
+            <div class="tarjetas">
+                <img src="./img/Imagen1.jpg" alt="foto1">
+            </div>
+            <div class="flechitas">
+                <a href="#carrusel_foto3">
+                    <i> < </i>
+                </a>
+                <a href="#carrusel_foto2">
+                    <i> > </i>
+                </a>
+            </div>                   
+        </div>
+        <div class="item_carrusel" id="carrusel_foto2">
+            <div class="tarjetas">
+                <img src="./img/Imagen2.jpg" alt="foto2">
+            </div>
+            <div class="flechitas">
+                <a href="#carrusel_foto1">
+                    <i> < </i>
+                </a>
+                <a href="#carrusel_foto3">
+                    <i> > </i>
+                </a>
+            </div>                   
+        </div>
+        <div class="item_carrusel" id="carrusel_foto3">
+            <div class="tarjetas">
+                <img src="./img/imagen3.jpg" alt="foto3">
+            </div>
+            <div class="flechitas">
+                <a href="#carrusel_foto2">
+                    <i> < </i>
+                </a>
+                <a href="#carrusel_foto1">
+                    <i> > </i>
+                </a>
+            </div>                   
+        </div>
+    </div>
+    
+
+    
+    
+</section>
+
+</main>`)
